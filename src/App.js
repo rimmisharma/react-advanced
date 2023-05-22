a84ef2ea5bc6a8e41f27d419aca80b45edfe1b9c
rimmi <rimmi1187.cse19@chitkara.edu.in>
2023-05-22 12:01:11 +0530

    Implemented keys in react via a simple todo app

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
+import React, { useState } from 'react';
 
-/* Rendering lists in React */
-
-const topDesserts = [
-  {
-    id:"1",
-    title:"Tiramisu",
-    description:"The best tiramisu in town",
-    image:"https://sallysbakingaddiction.com/wp-content/uploads/2019/06/homemade-tiramisu-2.jpg",
-    price:"$5.00",
-  },
-  {
-    id:"2",
-    title:"Lemon Ice Cream",
-    description:"Mind blowing taste",
-    image:"https://www.lemonblossoms.com/wp-content/uploads/2022/05/Lemon-Ice-Cream-S4.jpg",
-    price:"$4.50",
-  },
-  {
-    id:"3",
-    title:"Chocolate mousse",
-    description:"Unexplored flavour",
-    image:"https://marleysmenu.com/wp-content/uploads/2020/04/Boozy-Chocolate-Mousse-Featured-Image-1.jpg",
-    price:"$6.00",
-  },
-];
+const ToDo = props => (
+  <tr>
+    <td>
+      <label>{props.id}</label>
+    </td>
+    <td>
+      <input />
+    </td>
+    <td>
+      <label>{props.createdAt}</label>
+    </td>
+  </tr>
+);
 
 export default function App(){
-  const listItems = topDesserts.map(dessert => {
-    const itemImage = <img src ={dessert.image} alt={dessert.description} className="image"></img>
-    const itemText = `${dessert.title} - ${dessert.price}`
-    return <li>{itemImage}
-    {itemText}</li>
-  })
+  const [todos, setTodos] = useState([{
+    id:'todo1',
+    createdAt:'18:00',
+  }, {
+    id:'todo2',
+    createdAt:'20:30',
+  }]);
+
+  const reverseOrder = () => {
+    setTodos([...todos].reverse());
+  }
+
   return (
     <div>
-      <ul>
-        {listItems}
-      </ul>
+      <button onClick={reverseOrder}>Reverse</button>
+      <table>
+        <tbody>
+          {todos.map((todo,index) => (
+            <ToDo key ={todo.id} id={todo.id} createdAt={todo.createdAt} />
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
