316b818d35ffc02d242d7bcaa22a79cf7a52ae95
rimmi <rimmi1187.cse19@chitkara.edu.in>
2023-05-22 11:09:27 +0530

    Rendering lists using map method

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+/* Rendering lists in React */
+
+const topDesserts = [
+  {
+    id:"1",
+    title:"Tiramisu",
+    description:"The best tiramisu in town",
+    image:"https://sallysbakingaddiction.com/wp-content/uploads/2019/06/homemade-tiramisu-2.jpg",
+    price:"$5.00",
+  },
+  {
+    id:"2",
+    title:"Lemon Ice Cream",
+    description:"Mind blowing taste",
+    image:"https://www.lemonblossoms.com/wp-content/uploads/2022/05/Lemon-Ice-Cream-S4.jpg",
+    price:"$4.50",
+  },
+  {
+    id:"3",
+    title:"Chocolate mousse",
+    description:"Unexplored flavour",
+    image:"https://marleysmenu.com/wp-content/uploads/2020/04/Boozy-Chocolate-Mousse-Featured-Image-1.jpg",
+    price:"$6.00",
+  },
+];
+
+export default function App(){
+  const listItems = topDesserts.map(dessert => {
+    const itemImage = <img src ={dessert.image} alt={dessert.description} className="image"></img>
+    const itemText = `${dessert.title} - ${dessert.price}`
+    return <li>{itemImage}
+    {itemText}</li>
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {listItems}
+      </ul>
     </div>
-  );
+  )
 }
-
-export default App;
